e7b53153aa387f289a7407b8fd8232c6c8af9e48
Richard Adeyele <jcrichleeninetyeight@gmail.com>
2023-10-18 01:40:01 +0100

    Fix: Removed unused Router variable causing the build to fail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Counter from "./Counter";
 import NotFoundPage from "./404";
 import ErrorBoundary from "./ErrorBoundary";
