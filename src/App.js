e44ce7a30ae7187e1aec1b366ec97b17d919a361
Richard Adeyele <jcrichleeninetyeight@gmail.com>
2023-10-24 10:49:49 +0100

    Update: Added a preloader component to beautify the page while users wait for the screen to come up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Counter from "./pages/Counter";
 import NotFoundPage from "./pages/404";
 import ErrorBoundary from "./components/ErrorBoundary";
 import ErrorBoundaryTest from "./pages/ErrorBoundaryTest";
+import Preloader from "./components/Preloader";
+import "../src/assets/styles/index.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
   return (
     <div className="main">
-      <Routes>
-        <Route path="/" element={<Counter />} />
-        <Route path="/test-error" element={<ErrorBoundary />} />
-        <Route path="/error-boundary-test" element={<ErrorBoundaryTest />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path="/test-error" element={<ErrorBoundary />} />
+          <Route path="/error-boundary-test" element={<ErrorBoundaryTest />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      )}
     </div>
   );
 }
---
+++ src/components/Preloader.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Preloader.css"; // Import the CSS file for styling
+
+const Preloader = () => {
+  return (
+    <div class="kart-loader">
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
